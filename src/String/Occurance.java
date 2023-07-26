10bf1fe5cbcf431a950684fa3c72bb519de897e7
gobi13061994 <gobi13061994@gmail.com>
2023-07-26 16:44:25 +0530

    string

--- src/String/Occurance.java
+++ src/String/Occurance.java
@@ -5,15 +5,15 @@ import java.util.Map;
 
 public class Occurance {
     public static void main(String[] args) {
-        String s="gobi  goooobi";
-        Map<Character,Integer> map=new LinkedHashMap<>();
-        for (int i = 0; i <s.length(); i++) {
+        String s = "gobi  goooobi";
+        Map<Character, Integer> map = new LinkedHashMap<>();
+        for (int i = 0; i < s.length(); i++) {
             char charAt = s.charAt(i);
-            if (map.containsKey(charAt)){
+            if (map.containsKey(charAt)) {
                 Integer count = map.get(charAt);
-                map.put(charAt,count+1);
-            }else {
-                map.put(charAt,1);
+                map.put(charAt, count + 1);
+            } else {
+                map.put(charAt, 1);
             }
 
         }
